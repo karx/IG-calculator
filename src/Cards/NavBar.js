094d3735ac890116c67e5aa6dcfbc46a1ed650ec
Kartik <kartik.arora1214@gmail.com>
2019-09-11 16:21:19 +0530

    updated assets

--- src/Cards/NavBar.js
+++ src/Cards/NavBar.js
@@ -3,6 +3,6 @@ import "./NavBar.css";
 
 export default () => (
   <nav className="navbar navbar-light fixed-top shadow-sm navbar-style">
-    <img className="navbar-brand text-center" src="viragram-log-sm.png" alt=""/>
+    <img className="navbar-brand text-center brand-logo" src="logo-header.png" alt=""/>
   </nav>
 );
