a9a1541afc687b26849fc493d8795bbf7bc62691
Rahul Agarwal <agarwalrahul1996@gmail.com>
2019-09-11 18:22:07 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
             </div>
             <div class="footer">
                 <div class="copyright">
-                    <p>Copyright &copy;It's in house and on the house <a href="http://viragram.me">Viragram Media</a></p>
+                    <p>It's in house and on the house. Copyright &copy;<a href="http://viragram.me">Viragram Media</a></p>
                 </div>
             </div>
         </div>
