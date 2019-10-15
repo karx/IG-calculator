24b9dc4182c87703eaa58e5ff6d93a71702c331c
Kartik <kartik.arora1214@gmail.com>
2019-10-16 02:50:42 +0530

    logo update

--- src/Cards/NavBar.js
+++ src/Cards/NavBar.js
@@ -3,6 +3,6 @@ import "./NavBar.css";
 
 export default () => (
   <nav className="navbar navbar-light fixed-top shadow-sm navbar-style">
-    <img className="navbar-brand text-center brand-logo" src="logo-header.png" alt=""/>
+    <a href="https://viragram.me"> <img className="navbar-brand text-center brand-logo" src="small.png" alt=""/> </a>
   </nav>
 );
