a3e26eb694c5dea9441e660d51bd8cc89df5d6b9
Kartik <kartik.arora1214@gmail.com>
2019-10-21 01:51:18 +0530

    logo and links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InstagramApp from './views/InstagramApp/InstagramApp';
 import ErrorNotFound from './views/ErrorNotFound';
 
 import { BrowserRouter as Router, Route, Link, Redirect, Switch  } from "react-router-dom";
+import "./App.css";
 
 
 export default class App extends React.Component {
@@ -31,6 +32,11 @@ export default class App extends React.Component {
                 <div className="copyright">
                     <p>It's in house and on the house. Copyright &copy;<a href="http://viragram.me">Viragram Media</a></p>
                 </div>
+                <div className="logo-div center">
+                <a href="https://viragram.me" class="center-a"> <img className="navbar-brand text-center brand-logo" src="small.png" alt=""/> </a>
+
+                </div>
+
             </div>
         </div>
         );
--- src/Cards/NavBar.js
+++ src/Cards/NavBar.js
@@ -3,6 +3,7 @@ import "./NavBar.css";
 
 export default () => (
   <nav className="navbar navbar-light fixed-top shadow-sm navbar-style">
-    <a href="https://viragram.me"> <img className="navbar-brand text-center brand-logo" src="small.png" alt=""/> </a>
+    
+    <a href="https://insights.viragram.me" class="a-ref"> <img className="navbar-brand text-center brand-logo" src="ig_logo_black.png" alt=""/> </a>
   </nav>
 );
