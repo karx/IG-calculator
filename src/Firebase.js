e0b58926b688739af278b69c2131e6b4dece7edc
Kartik <kartik.arora1214@gmail.com>
2020-07-24 13:06:28 +0530

    new firebase db for rusk

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,14 @@ import firestore from 'firebase/firestore'
 
 const settings = { timestampsInSnapshots: true };
 var firebaseConfig = {
-    apiKey: "AIzaSyC4JK0jEIVaPO_UZVHo3qn0iqLfrlg-QhU",
-    authDomain: "viragram-me.firebaseapp.com",
-    databaseURL: "https://viragram-me.firebaseio.com",
-    projectId: "viragram-me",
-    storageBucket: "",
-    messagingSenderId: "904760671559",
-    appId: "1:904760671559:web:77bfcd39cdfd65bcf72062"
+    apiKey: "AIzaSyCBbez7qmZ5fVj-fLPYHTEVeJXivtlQPJ0",
+    authDomain: "ig-tool-rm.firebaseapp.com",
+    databaseURL: "https://ig-tool-rm.firebaseio.com",
+    projectId: "ig-tool-rm",
+    storageBucket: "ig-tool-rm.appspot.com",
+    messagingSenderId: "492451868539",
+    appId: "1:492451868539:web:444d5e4e2215e4b81e97de",
+    measurementId: "G-R6RVN9NYZG"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,3 +19,4 @@ firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings(settings);
 
 export default firebase;
+
