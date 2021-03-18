788dbbf64d9afd64aebddb2c552a8a032fdf4e86
Kartik <kartik.arora1214@gmail.com>
2021-03-18 13:52:12 +0530

    trigger deploy

--- netlify/functions/instapop.js
+++ netlify/functions/instapop.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
+    
     let data = await fetch('https://www.instagram.com/trippy/?_a=1');
     return {
         statusCode: 200,
