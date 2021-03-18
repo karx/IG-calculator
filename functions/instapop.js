c87a899528ba6b8a32102c2bfc6728b29c5e792b
Kartik <kartik.arora1214@gmail.com>
2021-03-18 23:24:20 +0530

    more fn logs

--- functions/instapop.js
+++ functions/instapop.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
     
     let data = await fetch('https://www.instagram.com/trippy/?__a=1');
-    console.log(data);
+    console.log({event});
+    console.log({data});
     return {
         statusCode: 200,
         body: JSON.stringify(data)
